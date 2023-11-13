8770d4c7bd3501b053b434a6d064802c5d7520b1
Owen Dorsey <owen.p.dorsey@gmail.com>
2023-11-13 08:48:36 -0500

    New Project

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('message', (messageData) => {
+        console.log("Received data:", messageData);
+        // Broadcast message to all clients
+        io.emit('scannerMessage', messageData);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
